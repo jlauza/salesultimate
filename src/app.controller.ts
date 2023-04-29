6c2f4128d428a885e9991262cbfe6e26499cb056
jlauza <lauzajayson@gmail.com>
2023-04-30 00:48:51 +0800

    installed nestjs app and use .gitignore

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private appService: AppService) {}
+
+  @Get()
+  root(@Res() res: Response) {
+    return res.render('index', {
+      message: this.appService.getIndex(),
+    });
+  }
+}
