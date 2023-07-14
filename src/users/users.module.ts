5a0bb4c338a6f5506fb3d4196dcd18e9a2f7960a
jlauza <lauzajayson@gmail.com>
2023-07-14 21:48:25 +0800

    added for feature

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserSchema } from "./users.model";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: "user", schema: UserSchema }])],
+  // exports: [UsersService],
   providers: [UsersService],
-  exports: [UsersService],
   controllers: [UsersController],
 })
 export class UsersModule {}
