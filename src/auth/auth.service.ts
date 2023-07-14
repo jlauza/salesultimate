ba01608ff8e92428722acce30c407b2b9c190bff
jlauza <lauzajayson@gmail.com>
2023-07-14 22:26:25 +0800

    auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,32 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
-import { JwtService } from '@nestjs/jwt';
+import {
+  Injectable,
+  UnauthorizedException,
+  NotAcceptableException,
+} from "@nestjs/common";
+import { UsersService } from "../users/users.service";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
   constructor(
-    private service: UsersService, 
-    private jwtService: JwtService
-    ) {}
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
-    const user = await this.service.findOne(username);
-    if (user && user.password !== pass) {
-      throw new UnauthorizedException();
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.getUser(username);
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!user) {
+      throw new UnauthorizedException("User not found");
+    } else if (user && passwordValid) {
+      return {
+        userId: user.id,
+        userName: user.username,
+      };
     }
-
-    const payload = { sub: user.userId, username: user.username };
+    const payload = { sub: user.id, userName: user.username };
 
     return {
       access_token: await this.jwtService.signAsync(payload),
