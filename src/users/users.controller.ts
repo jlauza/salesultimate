165f51350b2fee98b0ea976a8d259764b91dc6c3
jlauza <lauzajayson@gmail.com>
2023-07-14 22:07:21 +0800

    added bycryt and signup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import * as bcrypt from "bcrypt";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post("/signup")
+  async addUser(
+    @Body("password") userPassword: string,
+    @Body("username") userName: string
+  ) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(userPassword, saltOrRounds);
+    const result = await this.usersService.insertUser(userName, hashedPassword);
+
+    return {
+      msg: "User created successfully",
+      userId: result.id,
+      userName: result.username,
+    };
+  }
+}
