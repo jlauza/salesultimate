c47a12090ab44ddd20c4e6c439cc64533ff7a275
jlauza <lauzajayson@gmail.com>
2023-07-16 21:33:33 +0800

    new file session serialize user

---
+++ src/auth/session.serializer.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { PassportSerializer } from "@nestjs/passport";
+
+@Injectable()
+export class SessionSerializer extends PassportSerializer {
+  serializeUser(user: any, done: (err: Error, user: any) => void): any {
+    console.log("serializeUser");
+    done(null, user);
+  }
+
+  deserializeUser(
+    payload: any,
+    done: (err: Error, payload: string) => void
+  ): any {
+    console.log("deserializeUser");
+    done(null, payload);
+  }
+}
