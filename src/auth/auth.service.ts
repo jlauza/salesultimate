bcd29b3d62505c4ca0c59f67e182161d6495c663
jlauza <lauzajayson@gmail.com>
2023-07-22 22:16:05 +0800

    added password args

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
-import { JwtService } from '@nestjs/jwt';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { UsersService } from "../users/users.service";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private service: UsersService, 
-    private jwtService: JwtService
-    ) {}
+  constructor(private service: UsersService, private jwtService: JwtService) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
-    const user = await this.service.findOne(username);
-    if (user && user.password !== pass) {
+  async signIn(username: string, password: string): Promise<any> {
+    const user = await this.service.findOne(username, password);
+    if (user && user.password !== password) {
       throw new UnauthorizedException();
     }
 
