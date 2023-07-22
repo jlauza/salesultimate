4c6ea1a1eadf4b0252bb10fddd9a66563ad508eb
jlauza <lauzajayson@gmail.com>
2023-07-22 21:38:35 +0800

    save

--- src/auth/local.auth.guard.ts
+++ src/auth/local.auth.guard.ts
@@ -2,7 +2,7 @@ import { ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard("jwt") {
+export class LocalAuthGuard extends AuthGuard("local") {
   async canActivate(context: ExecutionContext) {
     console.log(context);
     const result = (await super.canActivate(context)) as boolean;
