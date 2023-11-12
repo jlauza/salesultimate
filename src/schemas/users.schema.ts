4c6a5c4d6faa80ceb085a5ea55032f4a4a70fcc1
jlauza <lauzajayson@gmail.com>
2023-11-12 23:25:05 +0800

    added password

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    password : {
+        type: String,
+        required: true,
+    },
     createdAt: {
         type: Date,
         default: Date.now
