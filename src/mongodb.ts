4039979cc2a50106fe9a0f5f15502e3debc43d14
jlauza <lauzajayson@gmail.com>
2024-03-10 21:34:41 +0800

    corrected mongo uri value

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose, { ConnectOptions } from "mongoose";
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as ConnectOptions);
